7f9177622aa600f48fe9af5c854f9ea1d04963a7
Floofy <jimmycrabs2006@gmail.com>
2025-04-05 16:19:00 +0700

    a

--- backEnd/server.js
+++ backEnd/server.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // const express = require('express');
 var express_1 = require("express");
 var app = (0, express_1.default)();
-app.listen(5000, function () {
-    console.log("server loaded http://localhost:5000");
+app.listen(3000, function () {
+    console.log("server loaded http://localhost:3000");
 });
--- backEnd/server.ts
+++ backEnd/server.ts
@@ -1,6 +1,6 @@
 // const express = require('express');
 import express from "express"
 const app = express();
-app.listen(5000, () => {
-    console.log("server loaded http://localhost:5000")
+app.listen(3000, () => {
+    console.log("server loaded http://localhost:3000")
 })
